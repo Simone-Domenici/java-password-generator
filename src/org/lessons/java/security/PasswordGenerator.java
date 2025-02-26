05d4839aa0cdb7b7d25d05cd4ffd35d241fb01c6
Simone-Domenici <domenici.simone00@gmail.com>
2025-02-26 16:17:56 +0100

    Refactor PasswordGenerator to accept user input for password generation

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,16 +1,32 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        String Name = "John";
-        String Surname = "Doe";
-        String FavouriteColor = "Lime";
-        int BirthDay = 11;
-        int BirthMonth = 04;
-        int BirthYear = 2000;
+        String Name;
+        String Surname;
+        String FavouriteColor;
+        int BirthDay;
+        int BirthMonth;
+        int BirthYear;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        Name = in.nextLine();
+        System.out.println("Enter your surname: ");
+        Surname = in.nextLine();
+        System.out.println("Enter your favourite color: ");
+        FavouriteColor = in.nextLine();
+        System.out.println("Enter your birth day: ");
+        BirthDay = in.nextInt();
+        System.out.println("Enter your birth month: ");
+        BirthMonth = in.nextInt();
+        System.out.println("Enter your birth year: ");
+        BirthYear = in.nextInt();
         
         String Password = Name + "-" + Surname + "-" + FavouriteColor + "-" + (BirthDay + BirthMonth + BirthYear);
-        System.out.println(Password);
+        System.out.println("Your password is: " + Password);
 
     }
 }
